2f348d72a4fecad9450e5877b5dc21a8694ba8b9
Jehree <126540981+Jehree@users.noreply.github.com>
2024-05-30 00:58:29 -0400

    Update Plugin.cs
    comment cleanup

--- Plugin.cs
+++ Plugin.cs
@@ -9,17 +9,20 @@ using System.Threading.Tasks;
 
 namespace YourModName
 {
-    [BepInPlugin("YourUsername.YourModName", "YourModName", "0.0.1")] //first string MUST be unique to any other mod. Read more about it in BepInEx docs. 
+    //first string below is your plugin's GUID, it MUST be unique to any other mod. Read more about it in BepInEx docs. Be sure to update it if you copy this code.
+    [BepInPlugin("YourUsername.YourModName", "YourModName", "0.0.1")]  
     public class Plugin : BaseUnityPlugin
     {
         public static ManualLogSource LogSource;
 
-        private void Awake()
+        //BaseUnityPlugin inherits MonoBehaviour, so you can use base unity functions like Awake() and Update()
+        private void Awake() //Awake() will run once when your plugin loads
         {
             //we save the Logger to our LogSource variable so we can use it anywhere, such as in our patches via Plugin.LogSource.LogInfo(), etc.
-            LogSource = Logger; 
+            LogSource = Logger;
+            LogSource.LogInfo("plugin loaded!");
 
-            //uncomment the line below and replace "PatchClassName" with the class name you gave your patch. Patches must be enabled like this to do anything.
+            //uncomment the line below and replace "PatchClassName" with the class name you gave your patch. Patches must be enabled like this to work.
             //new PatchClassName().Enable();
         }
     }
