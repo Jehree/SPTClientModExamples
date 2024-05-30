b55d1e0ec9ccfa993b96a5f8f3d213632f696b33
Jehree <126540981+Jehree@users.noreply.github.com>
2024-05-30 00:34:19 -0400

    Create FiniteJumpPatch.cs

---
+++ Patches/FiniteJumpPatch.cs
@@ -0,0 +1,55 @@
+using Aki.Reflection.Patching;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using EFT;
+using System.Runtime.CompilerServices;
+
+namespace YourModName.Patches {
+    internal class FiniteJumpPatch : ModulePatch //we must inherit ModulePatch for our patch to work.
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            //methods are patched by targeting both their class name and the name of the method itself.
+            return AccessTools.Method(typeof(Player), nameof(Player.Jump));
+        }
+
+        public static int maxJumps = 3;
+        public static int completedJumps = 0;
+
+        [PatchPrefix]
+        static bool Prefix()
+        {
+            //code here will run BEFORE original code is executed.
+            //if 'true' is returned, the original code will still run.
+            //if 'false' is returned, the original code will be skipped.
+
+            if ( completedJumps <= maxJumps) {
+                completedJumps++;
+                int remainingJumps = maxJumps - completedJumps;
+
+                //here we are using that LogSource variable we set up in the Plugin.cs file.
+                Plugin.LogSource.LogWarning($"You jumped! You have {remainingJumps} left!"); //LogWarning() will log with a yellow tint.
+
+                return true; //here we return true to run the original code and jump!
+            } else {
+                //this isn't actually an error, LogError() will just log with a red color which is easier to spot in the BepInEx log.
+                Plugin.LogSource.LogError("You have no jumps left!");
+
+                return false; //here we are out of jumps, so we return false, preventing the original code, and thus, the jump.
+            }
+        }
+
+        [PatchPrefix]
+        static void Postfix()
+        {
+            //code here will run AFTER the original code is executed.
+        }
+
+      //don't forget to add FiniteJumpPatch().Enable(); to the Awake() method of your Plugin.cs script.
+    }
+}
